ffe74ad6af355b9cc46f752e3e468875b3f6fddd
Deepak Parajuli <bindaaspratiks@gmail.com>
2022-01-06 19:56:10 +0100

    controls and interaction added

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 import './style.css';
 import { Map, View } from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
+
 import { Control, defaults as defaultControls } from 'ol/control';
+import Draw from 'ol/interaction/Draw';
+import 'ol/ol.css';
+import { OSM, Vector as VectorSource } from 'ol/source';
+import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
 
-
-
-//control
 class RotateNorthControl extends Control {
     /**
      * @param {Object} [opt_options] Control options.
@@ -15,7 +15,7 @@ class RotateNorthControl extends Control {
         const options = opt_options || {};
 
         const button = document.createElement('button');
-        button.innerHTML = 'N';
+        button.innerHTML = '<i class="fas fa-map-pin"></i>';
 
         const element = document.createElement('div');
         element.className = 'rotate-north ol-unselectable ol-control';
@@ -36,16 +36,76 @@ class RotateNorthControl extends Control {
     }
 }
 
+const raster = new TileLayer({
+    source: new OSM(),
+});
+
+const source = new VectorSource({ wrapX: false });
+
+const vector = new VectorLayer({
+    source: source,
+});
+
 const map = new Map({
     controls: defaultControls().extend([new RotateNorthControl()]),
+    layers: [raster, vector],
     target: 'map',
-    layers: [
-        new TileLayer({
-            source: new OSM()
-        })
-    ],
     view: new View({
-        center: ol.proj.fromLonLat([85, 28]),
-        zoom: 14
-    })
+        center: [-11000000, 4600000],
+        zoom: 4,
+    }),
 });
+
+const typeSelect = $('#type')[0];
+
+$(".card-link1").click(function() {
+    if ((".card-link1")[0]) {
+        const value = "Point";
+        console.log(value);
+    }
+});
+$(".card-link2").click(function() {
+    if ((".card-link2")[0]) {
+        const value = "Line";
+        console.log(value);
+    }
+});
+
+$(".card-link3").click(function() {
+    if ((".card-link1")[0]) {
+        const value = "Polygon";
+        console.log(value);
+    }
+});
+
+
+let draw; // global so we can remove it later
+function addInteraction() {
+    const value = typeSelect.value;
+    if (value !== 'None') {
+        draw = new Draw({
+            source: source,
+            type: typeSelect.value,
+        });
+        map.addInteraction(draw);
+    }
+}
+
+/**
+ * Handle change event.
+ */
+typeSelect.onchange = function() {
+    map.removeInteraction(draw);
+    addInteraction();
+};
+
+document.getElementById('undo').addEventListener('click', function() {
+    draw.removeLastPoint();
+});
+
+addInteraction();
+
+
+
+
+//control
